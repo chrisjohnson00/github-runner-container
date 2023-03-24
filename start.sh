fed6bf377a37a06a4d3dc34300ec7ef376a20f4f
Chris Johnson <chrisjohnson00@hotmail.com>
2023-03-24 14:57:51 -0700

    fix: register at the repo level

--- start.sh
+++ start.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 ORGANIZATION=$ORGANIZATION
+REPO=$REPO
 ACCESS_TOKEN=$ACCESS_TOKEN
 
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+REG_TOKEN=$(curl -sX POST -H "Authorization: Bearer ${ACCESS_TOKEN}" https://api.github.com/repos/${ORGANIZATION}/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
-./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
+./config.sh --url https://github.com/${ORGANIZATION}/${REPO} --token ${REG_TOKEN}
 
 cleanup() {
     echo "Removing runner..."
