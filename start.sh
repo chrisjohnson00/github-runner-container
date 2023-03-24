4b8897462336166e7a42e97718833f61f969bcba
Chris Johnson <chrisjohnson00@hotmail.com>
2023-03-24 15:15:00 -0700

    chore: add labels and runner name

--- start.sh
+++ start.sh
@@ -3,12 +3,14 @@
 ORGANIZATION=$ORGANIZATION
 REPO=$REPO
 ACCESS_TOKEN=$ACCESS_TOKEN
+LABELS=$LABELS
+$RUNNER_NAME=$RUNNER_NAME
 
 REG_TOKEN=$(curl -sX POST -H "Authorization: Bearer ${ACCESS_TOKEN}" https://api.github.com/repos/${ORGANIZATION}/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
-./config.sh --url https://github.com/${ORGANIZATION}/${REPO} --token ${REG_TOKEN}
+./config.sh --url https://github.com/${ORGANIZATION}/${REPO} --token ${REG_TOKEN} --unattended --labels ${LABELS} --name ${RUNNER_NAME}
 
 cleanup() {
     echo "Removing runner..."
